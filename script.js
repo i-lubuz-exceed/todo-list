45407475d5e1aac19f9ef16bc3ff81f13471e1c3
labuz.ivan <i.lubuz.exceedteam@gmail.com>
2020-02-20 13:18:29 +0300

    finish version

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 $(function(){
-    $(".buttonForm").click(function(){ 
-        var r=Math.floor(Math.random() * (256));
-        var g=Math.floor(Math.random() * (256));
-        var b=Math.floor(Math.random() * (256));
-        var randomcolor='#' + r.toString(16) + g.toString(16) + b.toString(16);
-        var enteredText = $("#myInput").val();
-        $("#list").prepend(`<li class= "listli"><input id="checkbox" type="checkbox"/><div>${enteredText}</div></li>`);
+    $(".buttonForm").click(function(){
+        const massivecolor = ["#F16D69","#F379A2","#9170CB","#5EB3F6","#67D7E5","#FFE083"]
+        const randomcolor = massivecolor[Math.floor(Math.random() * massivecolor.length)];
+        const enteredText = $("#myInput").val();
+        $("#list").prepend(`<li class= "listli"><div id="bordfix"><input id="checkbox" type="checkbox"/></div><div id="listdiv">${enteredText}</div></li>`);
         $(".listli:eq(0)").css("background-color", randomcolor)
     })
     $(".box").click(function(){
-        var colorbox = $(this).css("background-color")
+        const colorbox = $(this).css("background-color")
         $("input:checkbox:checked").each(function(){
-            $(this).parent().css("background-color", colorbox)
+            $(this).parent().parent().css("background-color", colorbox)
         });
     })
 });
